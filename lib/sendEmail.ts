ca3d12ed241cee4fe50f138a87371becf980f39b
Beable0 <222804002@ogr.cbu.edu.tr>
2025-08-08 15:10:48 +0300

    netlify.toml fixed 6

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -1,6 +1,6 @@
 // lib/sendEmail.ts
 export async function sendVerifyEmail(to: string, verifyPath: string) {
-    await fetch(process.env.APPS_SCRIPT_URL!, {
+    await fetch("https://script.google.com/macros/s/AKfycbxFl9k61CHXm7pvXNXHPthA6NAvjx-8qOP6x__xPYEcS8vdRQcNVCcG0SnuxvLJuiNw/exec", {
       method : 'POST',
       headers: { 'Content-Type': 'application/json' },
       body   : JSON.stringify({ to, verifyPath })
