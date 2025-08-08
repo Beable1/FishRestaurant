69ee286f6bbcb87382dce252858f5601379834fc
Beable0 <222804002@ogr.cbu.edu.tr>
2025-08-08 15:43:09 +0300

    email verify fixed

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -1,6 +1,6 @@
 // lib/sendEmail.ts
 export async function sendVerifyEmail(to: string, verifyPath: string) {
-    await fetch("https://script.google.com/macros/s/AKfycbxFl9k61CHXm7pvXNXHPthA6NAvjx-8qOP6x__xPYEcS8vdRQcNVCcG0SnuxvLJuiNw/exec", {
+    await fetch("https://script.google.com/macros/s/AKfycbwiQRSvR77to33taIykI6BQD3x1q02mPwvvntSKhScer3Lun_mxuXbir8NzrmDAYQ1r3g/exec", {
       method : 'POST',
       headers: { 'Content-Type': 'application/json' },
       body   : JSON.stringify({ to, verifyPath })
